46bb8aa42900009ed2442db229bc03fb1fc94bdd
Faridah <faridahdada@outlook.com>
2021-02-22 18:20:43 +0000

    started exercise 12

---
+++ Exercise12.py
@@ -0,0 +1,15 @@
+cheese = ['Cheddar', 'Stiliton', 'Cornish yarg']
+cheese.append('oke')
+print(cheese)
+
+#How should Oke be added to the end of the cheese list ; see above
+
+#How would you add two cheese to the list with a single command
+cheese.extend(['Tesco value cheese', 'baybel'])
+print(cheese)
+
+#Question two
+tup = ('Hello') #counting the length of one item in the tuple
+print(len(tup))
+tup = ('Hello',) #counting how many items are in the tuple
+print(len(tup))
